a91ebccda2eacfcb3ad606dcb7d184b1ea8aafbe
Kestrel <41704630+BlaSfaiMe@users.noreply.github.com>
2020-06-11 10:03:49 +0200

    Edit for password and username

--- namegenerator.js
+++ namegenerator.js
@@ -7,23 +7,17 @@
 ------------------------------------------------------
 */
 
-function getPassword(length){
+function getPassword(name){
 	let char = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t"]
 	let number = ["1","2","3","4","5","6","7","8","9"]
-	let pass = "";
-	for(i = 0; i < length; i++){
-		let charornum = Math.floor(Math.random() * 2)
-		let lowerorhigh = Math.floor(Math.random() * 2)
-		if(charornum){
-			if(lowerorhigh){
-				pass += char[Math.floor(Math.random() * char.length)].toUpperCase()
-			} else {
-				pass += char[Math.floor(Math.random() * char.length)]
-			}
-		} else {
-			pass += number[Math.floor(Math.random() * number.length)]
-		}
+	let nameSplit = name.split('')
+	let name4pass = nameSplit[0]+nameSplit[1]+nameSplit[2];
+	let pass = name4pass.toUpperCase()+"_";
+	for(i = 0; i < 6; i++){
+		let num = Math.floor(Math.random() * 9)
+		pass += num
 	}
+	pass += "#"
 	return pass
 }
 const readline = require('readline')
@@ -39,8 +33,10 @@ rl.question("Combien d'utilisateur voulez vous créer ?", (n) => {
 	for(o = 0; o < n; o++){
 		prenom = prenoms[Math.floor(Math.random() * prenoms.length)]
 		nom = noms[Math.floor(Math.random() * noms.length)]
-		let password = getPassword(16)
-		console.log(`Utilisateur numéro ${o+1} ****************\nPrénom : ${prenom}\nNom : ${nom}\nNom d'utilisateur : ${prenom.toLowerCase()}.${nom.toLowerCase()}\nMot de passe : ${password}\n**********************************`)
+		let password = getPassword(nom)
+		prenomSplit = prenom.split("")
+		username = prenomSplit[0].toLowerCase()+"."+nom.toLowerCase()
+		console.log(`Utilisateur numéro ${o+1} ****************\nPrénom : ${prenom}\nNom : ${nom}\nNom d'utilisateur : ${username}\nMot de passe : ${password}\n**********************************`)
 	}
 	const millis = Date.now() - start;
 	console.log(`${n} utilisateurs créés en ${Math.floor(millis/1000)} s ${Math.floor(millis/100)}`);
